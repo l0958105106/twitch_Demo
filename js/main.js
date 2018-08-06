ea151bb3756701ad0c9a683f7b0c59304fb362d8
HuangGuanYu <l0958105106@gmail.com>
2018-08-06 18:24:00 +0800

    btnzhen

--- js/main.js
+++ js/main.js
@@ -1,26 +1,60 @@
-$(document).ready(function () {
-  $.ajax({
-    url: 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends',
-    headers: {'Client-ID': 'j57loptgkgakodmowbvqbqu92a61jy'},
-    type: 'GET',
-    success: function(data) {
-      for(var i = 0; i < 21; i++) {
-        let content =
-        `<a href="${data.streams[i].channel.url}" class='stream-item'>
-          <img class='preview' src="${data.streams[i].preview.large}">
-          <div class='content'>
-            <img class='avatar' src="${data.streams[i].channel.logo}">
-            <div class='stream-text'>
-              <p class='title'>${data.streams[i].channel.status}</p>
-              <p class='streamer'>${data.streams[i].channel.display_name}</p>
+$(document).ready(function (){
+  $("#btnZh").click(function (){
+    $.ajax({
+      url: 'https://api.twitch.tv/kraken/streams/?language=zh-tw',
+      headers: {'Client-ID': 'j57loptgkgakodmowbvqbqu92a61jy'},
+      dataType: 'json', 
+      type: 'GET',
+      success: function(data) {
+        console.log(data);
+        $(".container").empty();
+        for(var i = 0; i < 21; i++) {
+          let content =
+          `<a href="${data.streams[i].channel.url}" class='stream-item'>
+            <img class='preview' src="${data.streams[i].preview.medium}">
+            <div class='content'>
+              <img class='avatar' src="${data.streams[i].channel.logo}">
+              <div class='stream-text'>
+                <p class='title'>${data.streams[i].channel.status}</p>
+                <p class='streamer'>${data.streams[i].channel.display_name}</p>
+              </div>
             </div>
-          </div>
-        </a>`;
-        $( ".container" ).append(content);
+          </a>`;
+          $(".container").append(content);
+        }
+      },
+      error: function(data) {
+        console.log('error', data);
       }
-    },
-    error: function(data) {
-      console.log('error', data);
-    }
+    });
+  });
+  $("#btnKo").click(function (){
+    $.ajax({
+      url: 'https://api.twitch.tv/kraken/streams/?language=ko',
+      headers: {'Client-ID': 'j57loptgkgakodmowbvqbqu92a61jy'},
+      dataType: 'json', 
+      type: 'GET',
+      success: function(data) {
+        console.log(data);
+        $(".container").empty();
+        for(var i = 0; i < 21; i++) {
+          let content =
+          `<a href="${data.streams[i].channel.url}" class='stream-item'>
+            <img class='preview' src="${data.streams[i].preview.medium}">
+            <div class='content'>
+              <img class='avatar' src="${data.streams[i].channel.logo}">
+              <div class='stream-text'>
+                <p class='title'>${data.streams[i].channel.status}</p>
+                <p class='streamer'>${data.streams[i].channel.display_name}</p>
+              </div>
+            </div>
+          </a>`;
+          $(".container").append(content);
+        }
+      },
+      error: function(data) {
+        console.log('error', data);
+      }
+    });
   });
 });
