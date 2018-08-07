477fdd30cf4ffcbf3caa8169e7d5a7f9b63683e4
HuangGuanYu <l0958105106@gmail.com>
2018-08-07 18:53:13 +0800

    request change

--- js/main.js
+++ js/main.js
@@ -1,60 +1,40 @@
 $(document).ready(function (){
-  $("#btnZh").click(function (){
+  let lang_ko = 'https://api.twitch.tv/kraken/streams/?language=ko&game=League%20of%20Legends';
+  let lang_tw = 'https://api.twitch.tv/kraken/streams/?language=zh-tw&game=League%20of%20Legends';
+  let getAjax = function(language){
     $.ajax({
-      url: 'https://api.twitch.tv/kraken/streams/?language=zh-tw',
+      url: language,
       headers: {'Client-ID': 'j57loptgkgakodmowbvqbqu92a61jy'},
       dataType: 'json', 
       type: 'GET',
       success: function(data) {
         console.log(data);
         $(".container").empty();
-        for(var i = 0; i < 21; i++) {
+        for(let i = 0; i < 21; i++) {
           let content =
           `<a href="${data.streams[i].channel.url}" class='stream-item'>
             <img class='preview' src="${data.streams[i].preview.medium}">
-            <div class='content'>
-              <img class='avatar' src="${data.streams[i].channel.logo}">
-              <div class='stream-text'>
-                <p class='title'>${data.streams[i].channel.status}</p>
-                <p class='streamer'>${data.streams[i].channel.display_name}</p>
+              <div class='content'>
+                <img class='avatar' src="${data.streams[i].channel.logo}">
+                  <div class='stream-text'>
+                  <p class='title'>${data.streams[i].channel.status}</p>
+                  <p class='streamer'>${data.streams[i].channel.display_name}</p>
+                </div>
               </div>
-            </div>
-          </a>`;
+            </a>`;
           $(".container").append(content);
         }
       },
       error: function(data) {
         console.log('error', data);
-      }
-    });
+        }
+      });
+    }
+  $("#btnZh").click(function (){
+    getAjax(lang_tw);
   });
   $("#btnKo").click(function (){
-    $.ajax({
-      url: 'https://api.twitch.tv/kraken/streams/?language=ko',
-      headers: {'Client-ID': 'j57loptgkgakodmowbvqbqu92a61jy'},
-      dataType: 'json', 
-      type: 'GET',
-      success: function(data) {
-        console.log(data);
-        $(".container").empty();
-        for(var i = 0; i < 21; i++) {
-          let content =
-          `<a href="${data.streams[i].channel.url}" class='stream-item'>
-            <img class='preview' src="${data.streams[i].preview.medium}">
-            <div class='content'>
-              <img class='avatar' src="${data.streams[i].channel.logo}">
-              <div class='stream-text'>
-                <p class='title'>${data.streams[i].channel.status}</p>
-                <p class='streamer'>${data.streams[i].channel.display_name}</p>
-              </div>
-            </div>
-          </a>`;
-          $(".container").append(content);
-        }
-      },
-      error: function(data) {
-        console.log('error', data);
-      }
-    });
+    getAjax(lang_ko);
   });
+  window.onload = getAjax(lang_tw);
 });
